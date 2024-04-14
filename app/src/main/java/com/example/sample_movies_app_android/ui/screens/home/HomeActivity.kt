9f6e49485d88785d41148cb8c44c86a877954b1e
Hossam Rakha <hossamrakha0@gmail.com>
2024-04-14 19:02:53 +0200

    first commit

---
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeActivity.kt
@@ -0,0 +1,46 @@
+package com.example.sample_movies_app_android.ui.screens.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sample_movies_app_android.ui.theme.Sample_movies_app_androidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Sample_movies_app_androidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Sample_movies_app_androidTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.sample_movies_app_android.ui.screens.home
+
+class HomeViewModel {
+}
