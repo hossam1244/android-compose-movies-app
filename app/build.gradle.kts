09cd7005d5d6ef3dd28cef200653c2dee5a0d2ef
Hossam Rakha <hossamrakha0@gmail.com>
2024-04-21 19:16:15 +0200

    Added retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
