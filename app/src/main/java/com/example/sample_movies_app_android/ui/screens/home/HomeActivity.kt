6caaaf7db3ceca299e822f9e1bbe6fbace127491
Hossam Rakha <hossamrakha0@gmail.com>
2024-08-27 22:53:46 +0300

    adding if condition to compose example

--- app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeActivity.kt
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.sample_movies_app_android.ui.screens.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sample_movies_app_android.ui.theme.MoviesAppTheme
@@ -66,8 +72,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) {
                     Text(text = "Hello, World!")
-                    Text(text = "Hello, World!")
-                    Row {
+                    if (2 < 3) {
+                        Text(text = "Hello, World!")
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
                         Text(text = "Hello, World!")
                         Text(text = "Hello, World!")
                     }
