9639c0f34a1b32c9ace436e518f8c0d77e962318
Hossam Rakha <hossamrakha0@gmail.com>
2024-08-27 22:28:01 +0300

    playing around jetpack compose

--- app/src/main/java/com/example/sample_movies_app_android/MoviesApplication.kt
+++ app/src/main/java/com/example/sample_movies_app_android/MoviesApplication.kt
@@ -2,6 +2,27 @@ package com.example.sample_movies_app_android
 
 import android.app.Application
 
+/**
+ *  MVVM Architecture
+ *  Bottom Navigation Bar
+ *  Side Menu
+ *  Navigation Component
+ *  List of Movies
+ *  Movie Details
+ *  Movie Search
+ *  DB for Movies
+ *  Networking
+ *  Dependency Injection
+ *  Unit Testing
+ *  Coroutines
+ *  Data Binding
+ *  Flow
+ *  LiveData
+ *  View Binding
+ *  Room
+ *  Retrofit
+ *  Hilt
+ */
 class MoviesApplication : Application() {
 
     companion object {
--- app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeActivity.kt
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/home/HomeActivity.kt
@@ -3,13 +3,20 @@ package com.example.sample_movies_app_android.ui.screens.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sample_movies_app_android.ui.theme.MoviesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesAppTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.primary)
 
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(text = "Hello, World!")
+                        Text(text = "Hello, World!")
+                        Row {
+                            Text(text = "Hello, World!")
+                            Text(text = "Hello, World!")
+                        }
+                        LazyColumn {
+                            items(100) {
+                                Text(text = "Hello, World!")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    @Preview
+    fun PreviewHomeScreen() {
+        MoviesAppTheme {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(16.dp)
+
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = "Hello, World!")
+                    Text(text = "Hello, World!")
+                    Row {
+                        Text(text = "Hello, World!")
+                        Text(text = "Hello, World!")
+                    }
+                    LazyColumn {
+                        items(100) {
+                            Text(text = "Hello, World!")
+                        }
+                    }
+                }
             }
         }
     }
