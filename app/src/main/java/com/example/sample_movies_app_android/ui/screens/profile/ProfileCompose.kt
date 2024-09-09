40a40889e3fe293c51b2af03864f59f30963e329
Hossam Rakha <hossamrakha0@gmail.com>
2024-09-09 17:13:05 +0300

    created profile circle image

--- app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
@@ -1,9 +1,50 @@
 package com.example.sample_movies_app_android.ui.screens.profile
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 
 @Composable
 fun ProfileScreen() {
+    return Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Profile Screen")
+        // create image avatar
+        AsyncImage(
+            model = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH44asUcIXZa8taXFp2OlJzx7SGXDhwqDUsoD7NMgJ2Vj12CY0mV2QpgGUy_4FbKotJTA&usqp=CAU",
+            contentDescription = "Profile Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(150.dp)
+                .width(150.dp)
+                .border(
+                    BorderStroke(4.dp, MaterialTheme.colorScheme.primary),
+                    CircleShape
+                )
+                .padding(4.dp)
+                .clip(CircleShape)
+        )
 
+
+    }
 }
