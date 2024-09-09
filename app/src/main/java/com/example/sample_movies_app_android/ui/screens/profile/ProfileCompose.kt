d6c5b931b7ec1b3a546a821cae1898ad40f1a9bb
Hossam Rakha <hossamrakha0@gmail.com>
2024-09-09 22:11:14 +0300

    removed un-used code

--- app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,13 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -36,7 +30,6 @@ import coil.compose.AsyncImage
 import com.example.sample_movies_app_android.R
 import com.example.sample_movies_app_android.ui.component.CustomTopAppBar
 import com.example.sample_movies_app_android.ui.component.ProfileBoxItem
-import com.example.sample_movies_app_android.ui.screens.home.MoviesList
 
 
 @Composable
