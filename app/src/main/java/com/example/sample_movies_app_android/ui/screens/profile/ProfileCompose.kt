581f75f2a9576a52fe9638d0930e64fa123b451b
Hossam Rakha <hossamrakha0@gmail.com>
2024-09-09 17:33:30 +0300

    Created list of boxes for profile

--- app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
+++ app/src/main/java/com/example/sample_movies_app_android/ui/screens/profile/ProfileCompose.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +14,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -78,6 +86,59 @@ fun ProfileScreen() {
                     .fillMaxWidth()
                     .background(MaterialTheme.colorScheme.primary)
             )
+
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp))
+                    .padding(top = 20.dp, start = 20.dp, end = 20.dp)
+                    .fillMaxWidth()
+
+            ) {
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp))
+                        .background(MaterialTheme.colorScheme.primary)
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Default.Settings, contentDescription = "")
+                    Text(
+                        text = "Settings",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White
+                    )
+                    Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "")
+                }
+            }
+
+
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp))
+                    .padding(top = 20.dp, start = 20.dp, end = 20.dp)
+                    .fillMaxWidth()
+
+            ) {
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(15.dp, 15.dp, 15.dp, 15.dp))
+                        .background(MaterialTheme.colorScheme.primary)
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Default.Lock, contentDescription = "")
+                    Text(
+                        text = "Logout",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White
+                    )
+                    Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "")
+                }
+            }
 
 
         }
